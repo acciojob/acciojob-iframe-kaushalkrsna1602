bf0b33be0c895d47fac81a4b1c7827271eb75bcf
Kaushal Kumar <Kaushal.kr.kr@gmail.com>
2024-09-13 12:48:53 +0530

    created a submission

--- script.js
+++
@@ -1,7 +0,0 @@
-//your JS code here. If required.
-cy.get("iframe#my-iframe")
-  .wait(5000)
-  .should("be.visible")
-  .and("have.attr", "src", "https://acciojob.com/")
-  .invoke('css', 'width')
-  .should('equal', '800px');
